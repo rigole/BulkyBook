76b983a3c80f4af78dc404f3388ccbe03394276b
rigole <foplacide@gmail.com>
2023-06-22 20:12:51 +0100

    adding and redising bootstrap

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController (ApplicationDbContext db)
+        {
+            dbContext = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = dbContext.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
